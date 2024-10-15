fb7b7499d75d1673a8129c262735a15ac9199aff
Blackshadow2492 <atuldhawale5678@gmail.com>
2024-10-15 20:15:18 +0530

    deleteby_id/getby_id/updateby_id

--- src/main/java/com/example/one_to_one/controller/DemoController.java
+++ src/main/java/com/example/one_to_one/controller/DemoController.java
@@ -4,9 +4,9 @@ import com.example.one_to_one.entity.Instructor;
 import com.example.one_to_one.entity.InstructorDetail;
 import com.example.one_to_one.repository.AppDao;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -21,5 +21,42 @@ public class DemoController {
         instructor.setInstructorDetail(instructorDetail);
 
         return appDao.saveInstructor(instructor);
+    }
+    @GetMapping("/getbyid/{instructorId}")
+    public Instructor getInstructorById(@PathVariable int instructorId) {
+        return appDao.getInstructorById(instructorId);
+    }
+    @GetMapping("/getall")
+    public List<Instructor> getAllInstructors() {
+        return appDao.getAllInstructors();
+    }
+
+    @DeleteMapping("/deletebyid/{instructorId}")
+    public String deleteByInstructorId(@PathVariable int instructorId) {
+        String message = appDao.deleteInstructorById(instructorId);
+
+        return message;
+
+    }
+
+    @PutMapping("/updatebyid/{instructorId}")
+    public Instructor updateByInstructorId(@PathVariable int instructorId, @RequestBody Instructor instructor) {
+        Instructor savedInstructor = appDao.getInstructorById(instructorId);
+        if (savedInstructor != null) {
+            InstructorDetail updatedInstructorDetail = instructor.getInstructorDetail();
+            updatedInstructorDetail.setInstructorDetailId(savedInstructor.getInstructorDetail().getInstructorDetailId());
+
+
+            appDao.saveInstructorDetail(updatedInstructorDetail);
+
+            instructor.setInstructorId(savedInstructor.getInstructorId());
+            instructor.setInstructorDetail(updatedInstructorDetail);
+
+            return appDao.saveInstructor(instructor);
+
+
+        } else {
+            return null;
+        }
     }
 }
--- src/main/java/com/example/one_to_one/repository/AppDao.java
+++ src/main/java/com/example/one_to_one/repository/AppDao.java
@@ -8,6 +8,8 @@ import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @AllArgsConstructor
 public class AppDao {
@@ -23,5 +25,50 @@ public class AppDao {
     public InstructorDetail saveInstructorDetail(InstructorDetail instructorDetail) {
         return  entityManager.merge(instructorDetail);
     }
+    public Instructor getInstructorById(int id) {
+        return entityManager.find(Instructor.class, id);
+    }
 
+    @Transactional
+    public String deleteInstructorById(int instructorId) {
+        Instructor instructor = getInstructorById(instructorId);
+        if (instructor != null) {
+            entityManager.remove(instructor);
+//            entityManager.remove(instructor.getInstructorDetail());
+            return "Deleted successfully";
+        } else {
+            return "Not Found!!";
+        }
+
+    }
+
+    public List<Instructor> getAllInstructors() {
+        List<Instructor> instructors = entityManager.createQuery("from Instructor").getResultList();
+
+        return instructors;
+    }
+
+//    @Transactional
+//    public Instructor updateInstructorById(int instructorId, Instructor instructor) {
+//
+//        Instructor instructor1 = getInstructorById(instructorId);
+//        InstructorDetail instructorDetail = instructor.getInstructorDetail();
+//        instructorDetail.setInstructorDetailId(instructor1.getInstructorDetail().getInstructorDetailId());
+//        if(instructor1 != null) {
+//            InstructorDetail updaInstructorDetail =  entityManager.merge(instructorDetail);
+//
+//            instructor.setInstructorDetail(updaInstructorDetail);
+//            Instructor updatedInstructor = entityManager.merge(instructor);
+//
+//            updatedInstructor.setInstructorDetail(updaInstructorDetail);
+//
+//            return updatedInstructor;
+//
+//        }else{
+//            return null;
+//        }
+//
+//    }
 }
+
+
